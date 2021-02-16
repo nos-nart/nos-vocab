6a3a5328dd06fa4e8fd34ea97d72d041b2b5b1db
nos-nart <nosnart1510@gmail.com>
2021-02-16 17:38:02 +0700

    feat: [+] gg

---
+++ src/layouts/AuthLayout.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Banner } from '@/components/svgs/Banner';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const AuthLayout = ({ children }: Props) => (
+  <>
+    <div className="w-full h-screen grid grid-cols-2">
+      <div className="cols-span-1 flex justify-end items-center">
+        <Banner width={500} height={500} />
+      </div>
+      <div className="cols-span-1 flex justify-center items-center">
+        {children}
+      </div>
+    </div>
+  </>
+);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import { AppProps } from 'next/app';
 import 'tailwindcss/tailwind.css';
+import PageWithLayoutType from '@/types/pageWithLayout';
+import { ThemeProvider } from '@/components/index';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+type AppLayoutProps = {
+  Component: PageWithLayoutType;
+  pageProps: any;
+};
+
+export default function MyApp({ Component, pageProps }: AppLayoutProps) {
+  const Layout = Component.layout || ((children: any) => <>{children}</>);
+
+  return (
+    <>
+      <ThemeProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
+  );
 }
